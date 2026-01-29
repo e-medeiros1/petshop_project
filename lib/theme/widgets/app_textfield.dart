37aed9a007336a4e4c72a75e1a3165c4b7759801
Erilândio Santos Medeiros <hsymetry@gmail.com>
2026-01-28 21:19:34 -0300

    Register page base

---
+++ lib/theme/widgets/app_textfield.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class AppTextfield extends StatefulWidget {
+  String name = '';
+  AppTextfield({super.key, required this.name});
+
+  @override
+  State<AppTextfield> createState() => _AppTextfieldState();
+}
+
+class _AppTextfieldState extends State<AppTextfield> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(decoration: InputDecoration(hint: Text(widget.name)));
+  }
+}
--- lib/views/auth/register/register_page.dart
+++ lib/views/auth/register/register_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:petshop_project/theme/colors.dart';
+import 'package:petshop_project/theme/widgets/app_textfield.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -11,8 +13,22 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Register page')),
-      body: Container(),
+      backgroundColor: AppColors.primary,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(child: Text('A logo vai aqui')),
+            SizedBox(height: 100),
+            AppTextfield(name: 'Digite seu e-mail'),
+            SizedBox(height: 10),
+            AppTextfield(name: 'Digite sua senha'),
+            SizedBox(height: 10),
+            AppTextfield(name: 'Confirme sua senha'),
+          ],
+        ),
+      ),
     );
   }
 }
